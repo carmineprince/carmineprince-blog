108b7c8f5bb4ca66cd4f2bbad77490c757fdd410
carmineprince <carmineprince@163.com>
2021-10-22 17:55:53 +0800

    feat: 重学前端笔记更新
    1. 关于类型，有那些细节
    2. JavaScript是面向对象还是基于对象

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -44,6 +44,18 @@ module.exports = [
     ],
   },
   {
+    text: "笔记",
+    link: "/note/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      {
+        items: [
+          { text: "重学前端", link: "/pages/d99d1f/" },
+        ],
+      },
+    ],
+  },
+  {
     text: "索引",
     link: "/archives/",
     items: [
